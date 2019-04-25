4cd9139b027925a191975f78ce475e2ff8713481
Mario Perez <secundariomario@gmail.com>
2019-04-25 16:24:23 +0200

    Metadata

--- Aplicaciones/MVC/AutobusesSese/Models/MetadatasRutas.cs
+++ Aplicaciones/MVC/AutobusesSese/Models/MetadatasRutas.cs
@@ -4,44 +4,31 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace AutobusesSese.Models.Metadatas
+namespace AutobusesSese.Models
 {
     [MetadataType(typeof(MetadatasRutas))]
     public partial class Rutas { }
     public class MetadatasRutas
     {
-        [Required]
         public int Id { get; set; }
 
-        [Required]
         [DataType(DataType.Text)]
         public int Origen { get; set; }
 
-        [Required]
         [DataType(DataType.Text)]
         public int Destino { get; set; }
 
-        
-        //[Range(0, 3,ErrorMessage ="madafakerhuevon")]
         [MaxLength(3)]
         public int Km { get; set; }
 
-
-        //[DataType(DataType.Time)]
-        [DataType(DataType.DateTime)]
-        //[DisplayFormat(DataFormatString = "{HH: mm}", ApplyFormatInEditMode = true)]
-        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
-        public System.DateTime Tiempo { get; set; }
-        //public System.TimeSpan Tiempo { get; set; }
+        public System.TimeSpan Tiempo { get; set; }
 
         [Range(0, 99999)]
         [DataType(DataType.Currency)]
         public int Precio { get; set; }
 
-        [Required]
         public virtual Ciudades Ciudades { get; set; }
 
-        [Required]
         public virtual Ciudades Ciudades1 { get; set; }
     }
 }
