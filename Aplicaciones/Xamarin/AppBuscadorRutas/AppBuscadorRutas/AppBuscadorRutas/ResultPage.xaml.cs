b0d8cec446b6f45e47f231c999cf2a06fd564cbb
Cesar Wilfrido Morales <cesar.morales@gruposese.com>
2019-04-25 15:02:34 +0200

    Modificaciones

--- Aplicaciones/Xamarin/AppBuscadorRutas/AppBuscadorRutas/AppBuscadorRutas/ResultPage.xaml.cs
+++ Aplicaciones/Xamarin/AppBuscadorRutas/AppBuscadorRutas/AppBuscadorRutas/ResultPage.xaml.cs
@@ -9,14 +9,11 @@ using Xamarin.Forms.Xaml;
 
 namespace AppBuscadorRutas
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ResultPage : ContentPage
 	{
 		public ResultPage ()
 		{
 			InitializeComponent ();
-            NavigationPage.SetHasBackButton(this, false);
-            ((NavigationPage)Application.Current.MainPage).BarBackgroundColor = Color.Black;
 		}
 	}
 }
