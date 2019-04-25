19b86424dfba91d990dc3cfe6ac0cefbf16d90c8
jorge.claver <jorge.claver@ESL00201>
2019-04-25 10:20:06 +0200

    mvc controllers y models

---
+++ Aplicaciones/MVC/AutobusesSese/Controllers/CiudadesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AutobusesSese.Models;
+
+namespace AutobusesSese.Controllers
+{
+    public class CiudadesController : Controller
+    {
+        private AutobusesSeseEntities1 db = new AutobusesSeseEntities1();
+
+        // GET: Ciudades
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Ciudades.ToListAsync());
+        }
+
+        // GET: Ciudades/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ciudades ciudades = await db.Ciudades.FindAsync(id);
+            if (ciudades == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ciudades);
+        }
+
+        // GET: Ciudades/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Ciudades/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,NombreCiudad")] Ciudades ciudades)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Ciudades.Add(ciudades);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(ciudades);
+        }
+
+        // GET: Ciudades/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ciudades ciudades = await db.Ciudades.FindAsync(id);
+            if (ciudades == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ciudades);
+        }
+
+        // POST: Ciudades/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,NombreCiudad")] Ciudades ciudades)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ciudades).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(ciudades);
+        }
+
+        // GET: Ciudades/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ciudades ciudades = await db.Ciudades.FindAsync(id);
+            if (ciudades == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ciudades);
+        }
+
+        // POST: Ciudades/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Ciudades ciudades = await db.Ciudades.FindAsync(id);
+            db.Ciudades.Remove(ciudades);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Aplicaciones/MVC/AutobusesSese/Controllers/RutasController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AutobusesSese.Models;
+
+namespace AutobusesSese.Controllers
+{
+    public class RutasController : Controller
+    {
+        private AutobusesSeseEntities1 db = new AutobusesSeseEntities1();
+
+        // GET: Rutas
+        public async Task<ActionResult> Index()
+        {
+            var rutas = db.Rutas.Include(r => r.Ciudades).Include(r => r.Ciudades1);
+            return View(await rutas.ToListAsync());
+        }
+
+        // GET: Rutas/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rutas rutas = await db.Rutas.FindAsync(id);
+            if (rutas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rutas);
+        }
+
+        // GET: Rutas/Create
+        public ActionResult Create()
+        {
+            ViewBag.Origen = new SelectList(db.Ciudades, "Id", "NombreCiudad");
+            ViewBag.Destino = new SelectList(db.Ciudades, "Id", "NombreCiudad");
+            return View();
+        }
+
+        // POST: Rutas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Origen,Destino,Km,Tiempo,Precio")] Rutas rutas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Rutas.Add(rutas);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Origen = new SelectList(db.Ciudades, "Id", "NombreCiudad", rutas.Origen);
+            ViewBag.Destino = new SelectList(db.Ciudades, "Id", "NombreCiudad", rutas.Destino);
+            return View(rutas);
+        }
+
+        // GET: Rutas/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rutas rutas = await db.Rutas.FindAsync(id);
+            if (rutas == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Origen = new SelectList(db.Ciudades, "Id", "NombreCiudad", rutas.Origen);
+            ViewBag.Destino = new SelectList(db.Ciudades, "Id", "NombreCiudad", rutas.Destino);
+            return View(rutas);
+        }
+
+        // POST: Rutas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Origen,Destino,Km,Tiempo,Precio")] Rutas rutas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(rutas).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Origen = new SelectList(db.Ciudades, "Id", "NombreCiudad", rutas.Origen);
+            ViewBag.Destino = new SelectList(db.Ciudades, "Id", "NombreCiudad", rutas.Destino);
+            return View(rutas);
+        }
+
+        // GET: Rutas/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rutas rutas = await db.Rutas.FindAsync(id);
+            if (rutas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rutas);
+        }
+
+        // POST: Rutas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Rutas rutas = await db.Rutas.FindAsync(id);
+            db.Rutas.Remove(rutas);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
