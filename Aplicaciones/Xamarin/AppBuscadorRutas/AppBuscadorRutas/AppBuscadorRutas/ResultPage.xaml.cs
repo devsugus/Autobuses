861bc31e62451d09e86010e23c526b5c12725290
Cesar Wilfrido Morales <cesar.morales@gruposese.com>
2019-04-25 13:49:22 +0200

    Añadida ResultPage y la barra de titulo

---
+++ Aplicaciones/Xamarin/AppBuscadorRutas/AppBuscadorRutas/AppBuscadorRutas/ResultPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppBuscadorRutas
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ResultPage : ContentPage
+	{
+		public ResultPage ()
+		{
+			InitializeComponent ();
+            NavigationPage.SetHasBackButton(this, false);
+            ((NavigationPage)Application.Current.MainPage).BarBackgroundColor = Color.Black;
+		}
+	}
+}
