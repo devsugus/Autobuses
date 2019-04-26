0a5a8d3e147b414d98a69f31bfa10cc8826f10f7
Mario Perez <secundariomario@gmail.com>
2019-04-26 10:29:47 +0200

    OData Funcional y completo, ser requiere extencin en las consultas para que salga en json

---
+++ Aplicaciones/OData/OData/OData/WCFDataServiceSesesvc.svc.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Data.Services
+Imports System.Data.Services.Common
+Imports System.Data.Services.Providers
+Imports System.Linq
+Imports System.ServiceModel.Web
+
+Public Class WCFDataServiceSesesvc
+    ' TODO: replace [[class name]] with your data class name
+    Inherits EntityFrameworkDataService(Of AutobusesSeseEntities)
+
+    ' This method is called only once to initialize service-wide policies.
+    Public Shared Sub InitializeService(ByVal config As DataServiceConfiguration)
+        ' TODO: set rules to indicate which entity sets and service operations are visible, updatable, etc.
+        ' Examples:
+        config.SetEntitySetAccessRule("*", EntitySetRights.AllRead)
+        ' config.SetServiceOperationAccessRule("MyServiceOperation", ServiceOperationRights.All)
+        config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V3
+    End Sub
+
+End Class
