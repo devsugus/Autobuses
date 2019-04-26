20828e1945136c17244223814aeca2a283f9a26b
sergio.abajo <sergio.abajo@ESL00203>
2019-04-26 15:23:19 +0200

    WebAPI

---
+++ Aplicaciones/WebApi/WebAPISeseAutobuses/WebAPISeseAutobuses/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace WebAPISeseAutobuses.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Aplicaciones/WebApi/WebAPISeseAutobuses/WebAPISeseAutobuses/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebAPISeseAutobuses.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
